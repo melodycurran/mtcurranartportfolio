fa40ac024070615fb299eaa24d060262986312f8
melodycurran <curranmelody02@gmail.com>
2026-07-12 17:04:18 -0600

    Changed the painting title

--- js/data.js
+++ js/data.js
@@ -1,7 +1,7 @@
 export const data = [{
 	"id": 0,
 	"image": "images/untitled.jpg",
-	"title": "Untitled",
+	"title": "Clothed Mannequin",
 	"dimensions": "9X12\"",
 	"medium": "Oil on Canvas",
 	"year": 2026
